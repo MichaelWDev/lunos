2f1d5520e7ea07f6de50eed503c3c7b803680c53
Michael Woodyard <mbwoodyard@hotmail.com>
2021-09-08 23:27:55 -0400

    Added entry page, vars. Implementing js/server

--- public/script.js
+++ public/script.js
@@ -2,16 +2,106 @@
 // SECTION Variables                     //
 //———————————————————————————————————————//
 
-
+const socket            = io();
+const messageContainer  = document.getElementById('chatted-words');
+const messageInput      = document.getElementById('message-bar');
+const usernameContainer = document.getElementById('usernames');
+let username            = prompt('What is your name?');
+// const profile          = document.getElementById('myImg');
 
 //———————————————————————————————————————//
 // SECTION Functions                     //
 //———————————————————————————————————————//
 
+// Enters the message upon hitting the enter key.
+function enterKey(e) {
+	if (e.keyCode === 13) {
+		const message = messageInput.value;
+		appendMessage(`You: ${message}`);
+		socket.emit('send-chat-message', message);
+		messageInput.value = '';
+	}
+}
 
+// Chat
+function appendMessage(message) {
+	const messageElement         = document.createElement('p');
+	messageElement.classList.add("text");
+	messageElement.innerText     = message;
+
+	messageContainer.insertBefore(messageElement, messageContainer.firstChild);
+}
+
+// Assigns all usernames to the right.
+function appendUsername(username) {
+	const userElement         = document.createElement('p');
+	userElement.id            = "userlist-" + username;
+	userElement.classList.add("user-list");
+	userElement.innerText     = username;
+
+	usernameContainer.insertBefore(userElement, usernameContainer.firstChild);
+}
+
+// Loop-Prompts the user until they provide a username.
+function promptUser() {
+	if (username === "" || !username || username.length < 3) {
+		username = prompt('What is your name?');
+		appendMessage('Please create a username with more than 3 characters.');
+	} else {
+		socket.emit('new-user', username);
+		clearInterval(promptInterval); // Clears the interval, so it stops looping.
+
+		// These used to just run on their own. Now waits for you to enter a proper username.
+		appendMessage('You joined');
+		appendUsername(username);
+	}
+}
+
+// Sends whichever channel is clicked to the back-end.
+function channelButton(button) {
+
+	switch(button) {
+		case 1:
+			socket.emit('channel-1');
+		break;
+
+		case 2:
+			socket.emit('channel-2');
+		break;
+
+		case 3:
+			socket.emit('channel-3');
+		break;
+
+		default:
+			socket.emit('channel-1');
+			// The default is the first channel.
+	}
+}
 
 //———————————————————————————————————————//
 // SECTION Sockets                       //
 //———————————————————————————————————————//
 
+socket.on('chat-message', data => {
+	appendMessage(`${data.name}: ${data.message}`);
+});
 
+socket.on('user-connected', username => {
+	appendMessage(`${username} connected.`);
+	console.log(username);
+	appendUsername(`${username}`);
+});
+
+socket.on('user-disconnected', username => {
+	appendMessage(`${username} disconnected.`);
+	document.getElementById("userlist-" + username).remove();
+});
+
+socket.on('user-list', users => {
+	for (socketId in users) {
+		appendUsername(users[socketId]);
+	}
+});
+
+let promptInterval = setInterval(promptUser, 5);
--- server.js
+++ server.js
@@ -1,9 +1,48 @@
+
+
+
 // Get readline module.
 const readline = require('readline').createInterface({input: process.stdin, output: process.stdout});
 
 // Create an Express app/
 const express = require("express");
 const app     = express();
+
+// Variables
+const server  = require("http").Server(app);
+const port    = 3000;
+const io      = require('socket.io')(server);
+const fs      = require('fs');
+const users   = {};
+let userList  = [];
+
+// Chat App
+io.on('connection', function(socket){
+	console.log("user connected");
+
+	socket.on('new-user', username => {
+		socket.emit('user-list', users); // Sends a full list of current users to the client when they join. (Minus their own.)
+		users[socket.id] = username; // users[socket.id] <<<< this is the username for a user.
+		socket.broadcast.emit('user-connected', username);
+		console.log(username); // this one does.
+	});
+
+	socket.on('send-chat-message', message => {
+		socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]});
+
+		fs.appendFile('message.txt', users[socket.id] + ": "+ message + "\n", function (err) {
+			if (err) throw err;
+			console.log('Saved!');
+		});
+	});
+	
+	socket.on('disconnect', () => {
+		socket.broadcast.emit('user-disconnected', users[socket.id]); // Emits username to client successfully.
+		console.log('user ' + users[socket.id] + ' disconnected');
+		userList.splice(userList.indexOf(socket.id), userList.indexOf(socket.id)); // Removes username from the right.
+		delete users[socket.id];
+  });
+});
 
 // Log errors.
 process.on('uncaughtException', function (err) {
@@ -13,11 +52,6 @@ process.on('uncaughtException', function (err) {
 
 // Use the Express-static middleware.
 app.use("/", express.static("public"));
-
-// Projects Page
-// Format: app.use("/PROJECT", express.static("projects/PROJECT"));
-// Projects: Clock, Password Generator,
-app.use("/clock", express.static("projects/clock"));
 
 // Start the server listening for requests.
 let server = app.listen(process.env.PORT || 3000, () => console.log("SERVER OPEN ON PORT " + (process.env.PORT || 3000)));
